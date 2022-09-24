b2443dd4d5b37da0f5503bf1d07ff997f9c13aef
luancostar <92955903+luancostar@users.noreply.github.com>
2022-09-24 01:05:03 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+import Navbar from "./components/Navbar/Navbar";
+import TaskList from "./components/TaskList/TaskList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div>
+        <TaskList title="Pendente" />
+        <TaskList title="Fazendo" />
+        <TaskList title="Completo" />
+      </div>
+    </div>
+  );
+}
